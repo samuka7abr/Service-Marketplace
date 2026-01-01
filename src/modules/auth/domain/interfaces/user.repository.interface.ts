79f6c103b100ca2f0059fe93bc5bf06b09fcf575
samuka7abr <samuelabrao2006@gmail.com>
2026-01-01 19:43:01 -0300

    :bug: fix(user): fixing uuid parameters in user repository interface

--- src/modules/auth/domain/interfaces/user.repository.interface.ts
+++ src/modules/auth/domain/interfaces/user.repository.interface.ts
@@ -4,7 +4,7 @@ export interface IUserRepository {
     create(user: User): Promise<void>;
     findByEmail(email: string): Promise<User | null>;
     findByUserName(userName: string): Promise<User | null>;
-    findById(id: string): Promise<User | null>;
+    findById(uuid: string): Promise<User | null>;
     update(user: User): Promise<void>;
-    delete(id: string): Promise<void>;
+    delete(uuid: string): Promise<void>;
 }
