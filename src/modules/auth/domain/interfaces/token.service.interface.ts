3d8788d23c7661f4a36807c060fbe32ac2710f30
samuka7abr <samuelabrao2006@gmail.com>
2026-01-03 21:40:18 -0300

    :bug: fix(auth): add JwtSignOptions to configure `expiresIn` field

--- src/modules/auth/domain/interfaces/token.service.interface.ts
+++ src/modules/auth/domain/interfaces/token.service.interface.ts
@@ -1,15 +1,16 @@
+import { JwtSignOptions } from '@nestjs/jwt';
 import { TokenPayload } from './token-payload.interface';
 
 export interface ITokenService {
     generateToken(
         payload: object,
-        expiresIn?: string | number,
+        expiresIn?: JwtSignOptions['expiresIn'],
     ): Promise<TokenPayload>;
     verifyToken(token: string): Promise<TokenPayload>;
     decodeToken(token: string): TokenPayload;
     generateRefreshToken(
         payload: object,
-        expiresIn?: string | number,
+        expiresIn?: JwtSignOptions['expiresIn'],
     ): Promise<TokenPayload>;
     verifyRefreshToken(token: string): Promise<TokenPayload>;
 }
