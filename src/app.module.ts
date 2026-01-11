16e93405ab0fffd1aa457f5492045e35ff43e5b5
samuka7abr <samuelabrao2006@gmail.com>
2026-01-11 20:42:12 -0300

    :heavy_plus_sign: chore(app): register UsersModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DynamoModule } from './database/dynamo.module';
+import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true }),
         DynamoModule,
+        UsersModule,
         AuthModule,
     ],
     controllers: [AppController],
